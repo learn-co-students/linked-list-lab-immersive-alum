06a5713c3e67ed70c27462e0c9b3cb2647485b1f
github username <github email address>
2017-08-16 12:22:30 -0400

    four tests passing

--- index.js
+++ index.js
@@ -3,7 +3,15 @@ function getName(node){
 }
 
 function headNode(node, collection){
-  console.log(node)
-  console.log(collection)
   return collection[node]
+}
+
+function next(node, collection){
+  let nextAddress = node.next
+  // console.log(node)
+  console.log(nextAddress)
+  // console.log(collection)
+  // console.log(collection[node])
+  return collection[nextAddress]
+
 }
