ae63fa116720cb0d7da1cf728c9ad4d7304fda53
github username <github email address>
2017-08-16 13:13:25 -0400

    seven tests passing

--- index.js
+++ index.js
@@ -18,3 +18,22 @@ function nodeAt(index, node, collection){
   }
   return currentNode
 }
+
+function addressAt(index, linkedList, collection){
+  if (index == 0){
+    return linkedList
+  } else {
+    let node = nodeAt(index - 1, linkedList, collection)
+    return node.next
+  }
+}
+
+function indexAt(node, collection, linkedList){
+  let currentNode = headNode(linkedList, collection)
+  let index = 0
+  while(currentNode != node){
+    index++
+    currentNode = next(currentNode, collection)
+  }
+  return index
+}
