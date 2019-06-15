12c015966c5827dd07fb3c60c42fa684cc2c05df
Kian Edalat <akedalat@gmail.com>
2019-06-15 13:41:10 +0100

    nodeAt

--- index.js
+++ index.js
@@ -9,3 +9,11 @@ return collection[linkedList]
 function next(head, collection){
 return collection[head.next]
 }
+
+function nodeAt(index, linkedList, collection){
+    let nextNode = collection[linkedList]
+    for(let i = 0; i < index; i++){
+        nextNode = collection[nextNode.next]
+    }
+    return nextNode
+}
